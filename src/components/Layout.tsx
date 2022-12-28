c2c1df8d6064ace6d30d50de1a8ca78eb45ca7a6
Jaewook Ahn <dev.jaewook@gmail.com>
2022-12-29 07:36:58 +0900

    fix: Layout background color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,12 +4,12 @@ import { Box } from '@chakra-ui/react';
 import { Header } from '../components'
 
 interface Props {
-  children: JSX.Element;
+  children: JSX.Element | JSX.Element[];
 }
 
 export const Layout = ({ children }: Props) => {
   return (
-    <Box width='100%' height='100vh' bg='#eee'>
+    <Box width='100%' minHeight='100vh' bg='#eee'>
       <Box mx='auto' maxWidth={768} minHeight='100%' bg='#fff'>
         <Header />
         {children}
