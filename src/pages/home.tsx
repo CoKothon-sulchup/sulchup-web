e9c76672c3c126a3d5bd6000fda11e8c770daa64
Jaewook Ahn <dev.jaewook@gmail.com>
2022-12-29 00:17:03 +0900

    feat(home): Write basic home page component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
+import { HiChevronRight } from 'react-icons/hi';
 
 export const Home = () => {
   return (
-    <div>
-      <h2>Home page</h2>
-    </div>
+    <Box display='flex' flexDirection='column' alignItems='center' pt='48px' px='32px'>
+      <Heading as='h1'>술첩</Heading>
+      <Box mt='24px' display='flex' flexDirection='row' alignItems='baseline'>
+        <Heading size='md'>최근 기록</Heading>
+        <Box display='flex' alignItems='center' cursor='pointer'>
+          <Text ml='12px'>더 보기</Text>
+          <HiChevronRight />
+        </Box>
+      </Box>
+      <Box mt='16px' display='flex' flexWrap='wrap' justifyContent='space-around'>
+        <Box m='8px' w='128px' h='192px' bg='#ccc' />
+        <Box m='8px' w='128px' h='192px' bg='#ccc' />
+        <Box m='8px' w='128px' h='192px' bg='#ccc' />
+      </Box>
+      <Button mt='24px' size='lg'>기록 추가</Button>
+    </Box>
   )
 };
