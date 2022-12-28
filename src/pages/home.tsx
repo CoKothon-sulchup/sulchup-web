cb4b5617661dba8f75b97ee478d001c8e63bec03
Jaewook Ahn <dev.jaewook@gmail.com>
2022-12-29 03:27:27 +0900

    improve(home): Update responsive style

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,9 +4,8 @@ import { HiChevronRight } from 'react-icons/hi';
 
 export const Home = () => {
   return (
-    <Box display='flex' flexDirection='column' alignItems='center' pt='48px' px='32px'>
-      <Heading as='h1'>술첩</Heading>
-      <Box mt='24px' display='flex' flexDirection='row' alignItems='baseline'>
+    <Box display='flex' flexDirection='column' alignItems='center' pt={['32px', '48px']} px={['24px', '48px']}>
+      <Box mt={['16px', '24px']} display='flex' flexDirection='row' alignItems='baseline' mr='auto'>
         <Heading size='md'>최근 기록</Heading>
         <Box display='flex' alignItems='center' cursor='pointer'>
           <Text ml='12px'>더 보기</Text>
@@ -14,6 +13,7 @@ export const Home = () => {
         </Box>
       </Box>
       <Box mt='16px' display='flex' flexWrap='wrap' justifyContent='space-around'>
+        <Box m='8px' w='128px' h='192px' bg='#ccc' />
         <Box m='8px' w='128px' h='192px' bg='#ccc' />
         <Box m='8px' w='128px' h='192px' bg='#ccc' />
         <Box m='8px' w='128px' h='192px' bg='#ccc' />
