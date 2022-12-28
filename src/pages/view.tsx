222f50b71515e68f7f97ffcbc2be19e36be5fe7a
kimho0304 <mfkimho0304@naver.com>
2022-12-29 03:29:30 +0900

    view beginning

--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -9,7 +9,16 @@ import {
   TagRightIcon,
   TagCloseButton,
 } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 
 export const View = () => {
-  return <div></div>;
+  return (
+    <Box display="flex">
+      <Box w="90%" h="100%" m="auto" bgColor="blue">
+        <Heading size="xl">Blue Moon</Heading>
+        <Image src="images/cocktail.png" w="70%" />
+      </Box>
+      <Box w="90%" h="100%" m="auto" bgColor="green"></Box>
+    </Box>
+  );
 };
