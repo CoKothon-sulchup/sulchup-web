48cc4dc38c718624c6d0b19ea5f51dec6b1707be
Jaewook Ahn <dev.jaewook@gmail.com>
2022-12-29 07:09:55 +0900

    fix(lint): unused var lint warnings

--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -1,14 +1,5 @@
 import { Box } from "@chakra-ui/react";
-import { Grid, GridItem } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
-import {
-  Tag,
-  TagLabel,
-  TagLeftIcon,
-  TagRightIcon,
-  TagCloseButton,
-} from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 
 export const View = () => {
