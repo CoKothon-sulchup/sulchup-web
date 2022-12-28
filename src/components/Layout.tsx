5f1d462a0ea5981447128193ff51094886181bad
Jaewook Ahn <dev.jaewook@gmail.com>
2022-12-29 00:56:21 +0900

    feat(core): Write and apply Header component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 
+import { Header } from '../components'
+
 interface Props {
   children: JSX.Element;
 }
@@ -9,6 +11,7 @@ export const Layout = ({ children }: Props) => {
   return (
     <Box width='100%' height='100vh' bg='#eee'>
       <Box mx='auto' maxWidth={768} minHeight='100%' bg='#fff'>
+        <Header />
         {children}
       </Box>
     </Box>
