1593322c4211df61bd7f87bd6bcd8b16f8316fcb
Jaewook Ahn <dev.jaewook@gmail.com>
2022-12-29 07:10:12 +0900

    feat: Add logo click handler in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,16 @@ import { BsSearch } from 'react-icons/bs';
 import { AiOutlineMenu } from 'react-icons/ai';
 
 export const Header = () => {
+  const handleLogoClick = () => {
+    window.location.href = '/';
+  };
+
   return (
     <Box w='100%' display='flex' alignItems='row' justifyContent='space-between' py='16px' px='32px'>
       <IconButton variant='ghost' aria-label='menu' icon={<AiOutlineMenu />} />
-      <Heading as='h1' size='lg'>술첩</Heading>
+      <Heading as='h1' size='lg' cursor='pointer' onClick={handleLogoClick}>
+        술첩
+      </Heading>
       <IconButton variant='ghost' aria-label='search' icon={<BsSearch />} />
     </Box>
   );
