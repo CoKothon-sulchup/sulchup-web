0906045073290c3ec35b52dbf1405e0ff982e261
Jaewook Ahn <dev.jaewook@gmail.com>
2022-12-29 11:42:40 +0900

    Improve write page

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
-import { Flex, Heading, IconButton, Input, Image, Textarea, Tabs, Tab, TabList, TabPanels, TabPanel } from '@chakra-ui/react';
-import { BsFillRecordCircleFill } from 'react-icons/bs';
+import React, { useEffect, useState } from 'react';
+import {
+  Flex,
+  Heading,
+  IconButton,
+  Input,
+  Image,
+  Textarea,
+  Grid,
+  Select,
+  GridItem,
+  Divider,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from '@chakra-ui/react';
+import { BsFillRecordCircleFill, BsFillStopCircleFill } from 'react-icons/bs';
+import { AiOutlineLeft } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { useReactMediaRecorder } from 'react-media-recorder';
+
+interface RecordingIconProps {
+  recording: boolean;
+}
+
+const RecordingIcon = (props: RecordingIconProps) => {
+  return !props.recording ? <BsFillRecordCircleFill size={32} /> : <BsFillStopCircleFill size={32} color='#f00' />;
+};
 
 interface InputPanelContainerProps {
   children: JSX.Element;
@@ -15,15 +42,77 @@ const InputPanelContainer = (props: InputPanelContainerProps) => {
   );
 };
 
+const today = new Date();
+
 export const Write = () => {
+  console.log(`date: ${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`)
+  const [drinkDate, setDrinkDate] = useState(`${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`);
+  const names = ["마신 장소", "도수", "같이 마신 사람", "가격"];
+
+  const handleInputChange = (setState: React.Dispatch<React.SetStateAction<any>>) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    return setState(e.target.value);
+  }
+
+  const gridItems = names.map((name) => (
+    <GridItem mb="10px">
+      <Heading size="md" mb="10px">
+        {name}
+      </Heading>
+      <Input w="90%" border="none" />
+      <Divider border="2px" w="90%" />
+    </GridItem>
+  ));
+
+  const onRecordingStart = () => {
+    console.log('recording started');
+  };
+
+  const onRecordingStop = () => {
+    console.log('recording stopped');
+  };
+
+  const navigate = useNavigate();
+  const {
+    status,
+    error,
+    mediaBlobUrl,
+    startRecording,
+    stopRecording,
+  } = useReactMediaRecorder({
+    audio: true,
+    askPermissionOnMount: true,
+    onStart: onRecordingStart,
+    onStop: onRecordingStop,
+  });
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
+  const toggleRecording = () => {
+    if (status === 'recording') {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      console.log('recorder error:', error);
+    }
+  }, [error]);
+
   return (
     <Flex w='100%' flexDirection='column' alignItems='center' pt='32px' px={['24px', '60px']}>
       <Heading mr='auto'>술첩 기록</Heading>
       <Flex mt='32px' w='100%'>
-        <Image w={['100px', '200px']} h={['100px', '200px']} src='https://via.placeholder.com/200' />
+        {/* <Image w={['100px', '200px']} h={['100px', '200px']} src='https://via.placeholder.com/200' /> */}
+        <Input w={['150px', '200px']} h={['150px', '200px']} type='file' />
+        {/* <Image w={['100px', '200px']} h={['100px', '200px']} src='https://via.placeholder.com/200' /> */}
         <Flex ml={['24px', '32px']} flex={1} flexDirection='column'>
           <Input placeholder='술 이름' />
-          <Input mt={['16px', '24px']} placeholder='마신 날짜' />
+          <Input mt={['16px', '24px']} type='date' placeholder='마신 날짜' value={drinkDate} onChange={handleInputChange(setDrinkDate)} />
         </Flex>
       </Flex>
       <Flex mt='48px' w='100%' flexDirection='column'>
